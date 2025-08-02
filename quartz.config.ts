b420426e9740c398109522f20095c4afb35a53cf
Omkar Kathi <omkarinbrn@gmail.com>
2025-08-02 15:20:13 +0530

    see if this works

--- quartz.config.ts
+++ quartz.config.ts
@@ -92,26 +92,6 @@ const config: QuartzConfig = {
       Plugin.Static(),
       Plugin.Favicon(),
       Plugin.NotFoundPage(),
-      // Comment out CustomOgImages to speed up build time
-      Plugin.CustomOgImages({
-        colorScheme: "lightMode", // what colors to use for generating image, same as theme colors from config, valid values are "darkMode" and "lightMode"
-        width: 1200, // width to generate with (in pixels)
-        height: 630, // height to generate with (in pixels)
-        excludeRoot: true, // wether to exclude "/" index path to be excluded from auto generated images (false = use auto, true = use default og image)
-        imageStructure: (options) => {
-          const element = Og(
-            options.cfg,
-            options.fileData,
-            options.userOpts.colorScheme,
-            options.title,
-            options.description,
-            options.fonts
-          );
-          // Provide a fallback empty element if Og returns null
-          return element ?? ({} as Element);
-        }, // custom image component to use
-
-      }),
     ],
   },
 }
--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -97,9 +97,9 @@ export default (() => {
 
         {!usesCustomOgImage && (
           <>
-            <meta property="og:image" content={ogImageDefaultPath} />
-            <meta property="og:image:url" content={ogImageDefaultPath} />
-            <meta name="twitter:image" content={ogImageDefaultPath} />
+            <meta property="og:image" content={fileData.frontmatter?.image} />
+            <meta property="og:image:url" content={fileData.frontmatter?.image} />
+            <meta name="twitter:image" content={fileData.frontmatter?.image} />
             <meta
               property="og:image:type"
               content={`image/${getFileExtension(ogImageDefaultPath) ?? "png"}`}
